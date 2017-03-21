558cd2fbf2894a08f987e0ed360cec8e4ef9ee76
Jackie Luc <jackie_luc@hotmail.com>
2017-03-21 14:11:03 -0600

    Initial template for a node project

---
+++ index.js
@@ -0,0 +1,15 @@
+var express = require('express');
+var app = express();
+var http = require('http').createServer(app);
+var io = require('socket.io')(http);
+var port = process.env.PORT || 3000;
+
+http.listen( port, function () {
+    console.log('listening on port', port);
+});
+
+app.use(express.static(__dirname + '/public'));
+
+io.on('connection', function(socket) {
+
+});
