c3b30c18ad93871746319d2af106beab3a40713c
Jackie Luc <jackie_luc@hotmail.com>
2017-03-30 15:33:29 -0600

    initial files - working socket.io communication between client and server

--- index.js
+++ index.js
@@ -8,8 +8,35 @@ http.listen( port, function () {
     console.log('listening on port', port);
 });
 
+app.get('/', function(req, res) {
+    res.sendFile('index.html', { root: './public' });
+});
+
+app.get('/spectate', function(req, res) {
+    res.sendFile('spectate.html', { root: './public' });
+});
+
 app.use(express.static(__dirname + '/public'));
+
+// =======================================================
+// Global Variables
+// =======================================================
+var USE_DEBUG_MSG = true;
+// var STATUSES = ['BOARD', 'NICKNAME', 'JUMP', 'WINNER', 'TURN', 'KING', 'LIST SPECTATORS', 'LIST'];
+
 
 io.on('connection', function(socket) {
 
+    socket.emit('BOARD', {test : 'hello123'});
+
 });
+
+// =======================================================
+// Functions
+// =======================================================
+
+// easy debug message generation
+function d(data) {
+    if (USE_DEBUG_MSG)
+        console.log(data);
+}
--- public/client.js
+++ public/client.js
@@ -1,5 +1,63 @@
+var BOARD = {};
+
 $(function() {
 
     var socket = io();
 
-}
+    socket.on('BOARD', function(data) {
+        updateBoard(data);
+    });
+
+});
+
+// =======================================================
+// Helper Functions
+// =======================================================
+
+// update the board
+function updateBoard(data) {
+    var rows = data.split('|');
+    var board = $('#board');
+    board.html(''); // Clear existing board contents
+
+    for (var i=0; i<rows.length; i++) {
+
+        var row = rows[i];
+        var pieces = row.split('');
+        var $row = $('<row>');
+        board.append($row);
+
+        for (var j=0; j<pieces.length; j++) {
+
+            var piece = pieces[j];
+            var $space = $('<space>');
+            $row.append($space);
+
+            if ((j + (i % 2)) % 2 != 0) {
+                $space.addClass('usable');
+                $space.on('dragover', allowDrop);
+                $space.on('drop', dropPiece);
+            }
+
+            var $piece = $('<piece>');
+
+            $piece.on('dragstart', dragPiece);
+            $piece.on('touchstart', touchDrag);
+            $piece.on('touchmove', touchMove);
+            $piece.on('touchend', touchDrop);
+
+            if (piece.toLowerCase() == 'r') {
+                $piece.addClass('red');
+            } else if (piece.toLowerCase() == 'b') {
+                $piece.addClass('black');
+            } else {
+                continue;
+            }
+            $space.append($piece);
+            if (piece >= 'A' && piece <= 'Z')
+                $piece.addClass('king');
+        }
+    }
+    $(getElement('game')).css('visibility', 'visible');
+    resizeGame();
+};
