08f07f41dfdf91e8b5e44898ba27fcc1768f4a9d
richardchan2 <richard.chan2@ucalgary.ca>
2017-04-03 20:45:52 -0600

    Created the checkerboard

--- index.js
+++ index.js
@@ -10,6 +10,19 @@ http.listen( port, function () {
 
 app.use(express.static(__dirname + '/public'));
 
+var nicknames = [];
+var numUsers = 0;
+
+
 io.on('connection', function(socket) {
+	console.log('A user connected');
+	socket.on('userConnected', function(data) {
+		numUsers++;
+		console.log(numUsers);
+		socket.nickname = "User " + numUsers;
+		nicknames.push(socket.nickname);
+		io.sockets.emit('usersOnline', nicknames);
+		io.sockets.emit('BOARD', 'BOARD');
+	});
 
 });
--- public/client.js
+++ public/client.js
@@ -1,5 +1,22 @@
 $(function() {
 
     var socket = io();
+	var $users = $('#users');
+	var $gameBoard = $('#gameBoard tBody');
 
-}
+	socket.emit('userConnected', 'New client has connected');
+	
+	socket.on('usersOnline', function(data) {
+		var html ='';
+		for(x = 0; x < data.length; x++) {
+			html = html + data[x] + "<br/>";
+		}
+		$users.html(html);
+	});
+	
+	socket.on('BOARD', function(data) {
+		var row = new Array(9).join('<td></td>');
+		var body = new Array(9).join('<tr>' + row + '</tr>');
+		$gameBoard.html(body);
+	});
+});
